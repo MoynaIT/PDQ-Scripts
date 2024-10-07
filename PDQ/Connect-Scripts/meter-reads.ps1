12f7bc7dcb0abc477ffaef6f15006b091977d1c4
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2024-10-07 11:01:43 -0500

    added meter reads script

---
+++ PDQ/Connect-Scripts/meter-reads.ps1
@@ -0,0 +1,13 @@
+# this script is setup for copy paste currently
+# set this ip to the ip of the printer
+$printer = 'someIPhere'
+$community = 'public'
+
+# this oid is to get the total printed pages
+$oid = '.1.3.6.1.2.1.43.10.2.1.4.1.1' 
+
+$SNMP = New-Object -ComObject olePrn.OleSNMP
+$SNMP.open($($printer),$($community),2,1000)
+$RESULT = $SNMP.get($($oid))
+$SNMP.Close()
+write-host "total pages " $RESULT
