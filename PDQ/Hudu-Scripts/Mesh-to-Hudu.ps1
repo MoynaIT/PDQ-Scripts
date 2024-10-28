52e66a287500e686290303719e1cbc2debc94940
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2024-10-28 09:57:45 -0500

    removed auth

--- PDQ/Hudu-Scripts/Mesh-to-Hudu.ps1
+++ PDQ/Hudu-Scripts/Mesh-to-Hudu.ps1
@@ -1,24 +1,10 @@
 # this script calls an N8N webhook with a little bit of data from meshagent.exe
 # the N8N function the calls the Hudu API and updates the asset with the link.
 
-# todo:
-# - figure out how to secure it without having the key in the code. Honestly there isn't much of a risk here,
-#   but it's a concern.
-
-#if (Get-Module -ListAvailable -Name HuduAPI) {
-#    Import-Module SecretManagement.KeePass 
-#}
-#else {
-#    Install-Module -name SecretManagement.KeePass -RequiredVersion 0.9.2 -Force -confirm:$true
-#    Import-Module SecretManagement.KeePass
-#}
-
 $meshAgent = "C:\Program Files\Mesh Agent\MeshAgent.exe"
 $meshParms = "-nodeid"
-$meshURL = (bws.exe secret get 87a6bf1c-3259-4b1a-844e-b21700e79c9e | convertfrom-Json).value
-$n8nURL = (bws.exe secret get 2f62489a-31a3-4e31-a088-b21700e75610 | convertfrom-Json).value
-$n8nToken = (bws.exe secret get d8facf10-af46-40bc-a12c-b21700e78148 | convertfrom-Json).value
-$headers = @{'authorization' = "Bearer $n8nToken" }
+$meshURL = (bws.exe secret get 87a6bf1c-3259-4b1a-844e-b21700e79c9e -t 0.2530736b-47f1-4d7d-9a72-b21700e80fd5.4JBQYJ0cVTq1gsp9DWf7wTZG6DwG55:t82GW6hga3hJS8O3WWvXKQ== | convertfrom-Json).value
+$n8nURL = (bws.exe secret get 2f62489a-31a3-4e31-a088-b21700e75610 -t 0.2530736b-47f1-4d7d-9a72-b21700e80fd5.4JBQYJ0cVTq1gsp9DWf7wTZG6DwG55:t82GW6hga3hJS8O3WWvXKQ== | convertfrom-Json).value
 
 # Asset Name
 $assetName = $ENV:COMPUTERNAME
@@ -32,4 +18,4 @@ $AssetFields = @{
     "meshagent" = "$meshFullURL"
 }
 
-$request = Invoke-WebRequest -Uri $n8nURL -Headers $headers -Method POST -Body $AssetFields
+$request = Invoke-WebRequest -Uri $n8nURL -Method POST -Body $AssetFields -UseBasicParsing
