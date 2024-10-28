792cf695678a4cebd58bc5810be1b450387b50e0
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2024-10-28 10:34:49 -0500

    rotated

--- PDQ/Hudu-Scripts/Mesh-to-Hudu.ps1
+++ PDQ/Hudu-Scripts/Mesh-to-Hudu.ps1
@@ -3,8 +3,8 @@
 
 $meshAgent = "C:\Program Files\Mesh Agent\MeshAgent.exe"
 $meshParms = "-nodeid"
-$meshURL = (bws.exe secret get 87a6bf1c-3259-4b1a-844e-b21700e79c9e -t 0.2530736b-47f1-4d7d-9a72-b21700e80fd5.4JBQYJ0cVTq1gsp9DWf7wTZG6DwG55:t82GW6hga3hJS8O3WWvXKQ== | convertfrom-Json).value
-$n8nURL = (bws.exe secret get 2f62489a-31a3-4e31-a088-b21700e75610 -t 0.2530736b-47f1-4d7d-9a72-b21700e80fd5.4JBQYJ0cVTq1gsp9DWf7wTZG6DwG55:t82GW6hga3hJS8O3WWvXKQ== | convertfrom-Json).value
+$meshURL = (bws.exe secret get 87a6bf1c-3259-4b1a-844e-b21700e79c9e | convertfrom-Json).value
+$n8nURL = (bws.exe secret get 2f62489a-31a3-4e31-a088-b21700e75610 | convertfrom-Json).value
 
 # Asset Name
 $assetName = $ENV:COMPUTERNAME
