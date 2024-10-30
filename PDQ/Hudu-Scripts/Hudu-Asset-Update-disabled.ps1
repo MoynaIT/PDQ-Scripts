855363ca2ee7bb30121e95d323387e28926a103a
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2024-10-30 10:04:41 -0500

    updated script to use bws and work with both companies

--- PDQ/Hudu-Scripts/Hudu-Asset-Update-disabled.ps1
+++ PDQ/Hudu-Scripts/Hudu-Asset-Update-disabled.ps1
@@ -6,11 +6,24 @@
 $HuduAPIKey = (bws.exe secret get 878de665-5841-4a37-a307-b21900f0b9ef | convertfrom-Json).value
 # Set the base domain of your Hudu instance without a trailing /
 $hududomain = (bws.exe secret get c9067ca6-0fa9-4922-ac57-b21900f234da | convertfrom-Json).value
-#Company Name as it appears in Hudu
-$CompanyName = (bws.exe secret get e42d2974-cde7-4bc5-bf13-b21900f27bf1 | convertfrom-Json).value
+# Asset Name as it stands in Hudu
 $HuduAssetLayoutName = "Computer Assets"
+# company name as it stand in Hudu
+# for me, this will be overridden below with our values
+$companyName = ""
 
 ###########################################################
+
+if ([string]::IsNullOrEmpty($companyName)) {
+    $domain = (get-computerinfo).CsDomain
+
+    if ($domain -eq ((bws.exe secret get 460505ce-1cf4-4e93-a334-b21900f58b5e | convertfrom-Json).value)) {
+        $companyName = (bws.exe secret get e42d2974-cde7-4bc5-bf13-b21900f27bf1 | convertfrom-Json).value
+    }
+    if ($domain -eq ((bws.exe secret get c49e8401-5d07-4f28-ba1b-b21900f59834 | convertfrom-Json).value)) {
+        $companyName = (bws.exe secret get 3dcaccdb-437e-4aa4-aee0-b21900f26721 | convertfrom-Json).value
+    }
+}
 
 # Get the Hudu API Module if not installed
 if (Get-Module -ListAvailable -Name HuduAPI) {
@@ -37,14 +50,16 @@ foreach ($computer in $ADComputers) {
     # Check if there is already an asset	
     # Asset Name
     $assetName = $computer.Name
-    write-host $assetName
     
     $HuduAsset = ($Assets | Where-Object {$_.name -like $assetName})
 
     if (!($HuduAsset)) {
         Continue
     }else {
-        Set-HuduAssetArchive -id $HuduAsset.id -CompanyId $companyid -archive $true -confirm:$false
+        if ($HuduAsset.archived -eq $false) {
+            write-host $assetName
+            Set-HuduAssetArchive -id $HuduAsset.id -CompanyId $companyid -archive $true -confirm:$false
+        }
     }
     sleep(.2)
 }
