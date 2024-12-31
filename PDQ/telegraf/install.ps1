432c28414b3ab1e41608a501c5ea10ac6fbc52c8
Patrick Palmersheim <ppalmersheim@cjmoyna.com>
2024-12-31 13:14:52 -0600

    added final steps

--- PDQ/telegraf/install.ps1
+++ PDQ/telegraf/install.ps1
@@ -13,3 +13,21 @@ If(!(test-path $path))
 {
       New-Item -ItemType Directory -Force -Path $path
 }
+
+# Victoria Metrics Settings
+$victoria_url = (bws.exe secret get c02559c0-e6da-4914-934c-b257013c2a8d | convertfrom-Json).value
+$victoria_database = "windows"
+
+# env registry key
+$telegraf_env =("victoria_url=$victoria_url","victoria_database=$victoria_database")
+
+# Create registry entries for telegraf
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\telegraf"-Name "Environment" -PropertyType MultiString -Value $telegraf_env -Force
+
+# Create and start telegraf service
+if (!(Get-Service telegraf)) {
+      & 'C:\Program Files\telegraf\telegraf.exe' --service install --config-directory 'C:\Program Files\telegraf\telegraf.d'
+  }
+  
+Set-Service -Name telegraf -Status Running -StartupType Automatic
+Restart-Service telegraf
